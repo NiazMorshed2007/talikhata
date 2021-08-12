30fef9472de86ab2cdb8547c9de07493ad7f9881
Niaz Morshed <niazmorshed2007@gmail.com>
2021-08-12 07:34:23 +0600

    first commit

--- js/app.js
+++ js/app.js
@@ -1,22 +1,32 @@
-var welcomeBtn = document.getElementById('welcome-btn');
-var welcomeInput = document.querySelectorAll('.welcome-third > label > input');
+var welcomeBtn = document.getElementById("welcome-btn");
+var setCodeBtn = document.getElementById("set-code-btn");
+var welcomeInput = document.querySelectorAll(".welcome-third > label > input");
+var setCodeInput = document.querySelectorAll(".set-code-third > label > input");
 
-welcomeInput.forEach(e => {
-    // e.setAttribute('readonly', readonly);
-    setInterval(() => {
-        welcomeBtnFunc(e);
-        btnBack();
-    }, 1000);
- });
+// setCodeInput.forEach((e, setCodeBtn) => {
+//   setInterval(() => {
+//     welcomeBtnFunc(e, setCodeBtn);
+//     btnBack();
+//   }, 1000);
+// });
+
+welcomeInput.forEach((e, welcomeBtn) => {
+  setInterval(() => {
+    welcomeBtnFunc(e);
+    btnBack();
+  }, 1000);
+});
 
 function welcomeBtnFunc(e) {
-    if(e.value === '') {
-        welcomeBtn.disabled = true;
-    } else {
-        welcomeBtn.disabled = false;
-    }
+  if (e.value === "") {
+    welcomeBtn.disabled = true;
+  } else {
+    welcomeBtn.disabled = false;
+  }
 }
 
 function btnBack() {
-    welcomeBtn.disabled ? welcomeBtn.classList.add('welcome-btn-disabled') :  welcomeBtn.classList.remove('welcome-btn-disabled');
+  welcomeBtn.disabled
+    ? welcomeBtn.classList.add("welcome-btn-disabled")
+    : welcomeBtn.classList.remove("welcome-btn-disabled");
 }
